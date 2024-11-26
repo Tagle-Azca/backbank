a8d41cd29e9319c78d17cce89dd775b3ae46e366
Andres Gomez Tagle Azcarraga <gtaglea@hotmail.com>
2024-11-26 00:46:28 -0600

    Configuración para Vercel

--- api/auth.js
+++ api/auth.js
@@ -1,33 +1,8 @@
-const connectToDatabase = require("../utils/db");
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express = require("express");
+const { login } = require("../controllers/authController");
 
-module.exports = async (req, res) => {
-  await connectToDatabase();
+const router = express.Router();
 
-  if (req.method === "POST") {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+router.post("/login", login);
 
-    if (!user) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Contraseña incorrecta" });
-    }
-
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
-    );
-
-    return res.status(200).json({ message: "Login exitoso", token });
-  }
-
-  return res.status(405).json({ message: "Método no permitido" });
-};
+module.exports = router;
--- api/users.js
+++ api/users.js
@@ -1,20 +1,8 @@
-const connectToDatabase = require("../utils/db");
-const User = require("../models/User");
+const express = require("express");
+const { getUsers } = require("../controllers/UserControllers");
 
-module.exports = async (req, res) => {
-  await connectToDatabase();
+const router = express.Router();
 
-  if (req.method === "GET") {
-    const users = await User.find();
-    return res.status(200).json(users);
-  }
+router.get("/", getUsers);
 
-  if (req.method === "POST") {
-    const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
-    await newUser.save();
-    return res.status(201).json(newUser);
-  }
-
-  return res.status(405).json({ message: "Método no permitido" });
-};
+module.exports = router;
