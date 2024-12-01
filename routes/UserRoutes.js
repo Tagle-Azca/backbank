52f739281a068e180862843d3ab16b1e9a7b33a0
Andres Gomez Tagle Azcarraga <gtaglea@hotmail.com>
2024-11-30 21:32:57 -0600

    Update pipeline for backend2

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// Endpoint para listar usuarios con rol 'cliente'
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find({ role: "cliente" }, "name rfc"); // Filtra por rol
-    res.status(200).json({ users }); // Devuelve un array de usuarios
+    const users = await User.find({ role: "cliente" }, "name rfc");
+    res.status(200).json({ users });
   } catch (error) {
     console.error("Error al obtener los usuarios:", error);
     res.status(500).json({ message: "Error al obtener los usuarios" });
