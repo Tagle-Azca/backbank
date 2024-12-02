3043f69c166b0da05dbbae0d7c02b4e5ab249da0
Andres Gomez Tagle Azcarraga <gtaglea@hotmail.com>
2024-12-01 23:25:19 -0600

    cambios para produccion pruebas2

--- controllers/__test__/authController.test.js
+++ controllers/__test__/authController.test.js
@@ -6,7 +6,16 @@ const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
+jest.mock("../../models/User", () => ({
+  findOne: jest.fn(),
+}));
+const User = require("../../models/User");
+
 describe("Auth Controller Tests with Mocked Logic", () => {
+  beforeEach(() => {
+    jest.clearAllMocks(); // Limpia los mocks antes de cada prueba
+  });
+
   it("should return 400 if email or password is missing", async () => {
     const response = await request(app)
       .post("/api/auth/login")
@@ -32,9 +41,15 @@ describe("Auth Controller Tests with Mocked Logic", () => {
   });
 
   it("should return 200 for valid email and password", async () => {
+    User.findOne.mockResolvedValue({
+      email: "rendon.a.la@gmail.com",
+      password: "$2b$10$hashedpassword", // Contraseña hasheada simulada
+    });
+
     const response = await request(app)
       .post("/api/auth/login")
       .send({ email: "rendon.a.la@gmail.com", password: "12345" });
+
     expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.objectContaining({
@@ -44,9 +59,12 @@ describe("Auth Controller Tests with Mocked Logic", () => {
   });
 
   it("should return 401 for invalid email or password", async () => {
+    User.findOne.mockResolvedValue(null); // Simula que el usuario no existe
+
     const response = await request(app)
       .post("/api/auth/login")
       .send({ email: "wrong@example.com", password: "wrongpassword" });
+
     expect(response.status).toBe(401);
     expect(response.body).toEqual(
       expect.objectContaining({
